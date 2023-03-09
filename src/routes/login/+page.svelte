1582450ad7364f2149a18cb9f6165c0ae959295c
danamestrial <chayapolbun@gmail.com>
2023-03-09 20:35:09 +0700

    feat: added framework for login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+	import LayoutGrid, { Cell } from '@smui/layout-grid';
+	import { Icon } from '@smui/common';
+	import IconButton from '@smui/icon-button';
+	import BottomAppBar, { Section } from '@smui-extra/bottom-app-bar';
+	let bottomAppBar: BottomAppBar;
+	// let secondaryColor: boolean = false;
+</script>
+
+<div class="login centered">
+	<div class="column" style="margin-left: 10%; margin-right: 10%;">
+		<div style="margin-bottom: 15px;">
+			<p>Login</p>
+		</div>
+
+		<div style="margin-bottom: 10px;">
+			<p2>Email Address</p2>
+		</div>
+
+		<div class="input-field">
+			<LayoutGrid>
+				<Cell span={1}>
+					<div class="center-stuff">
+						<Icon class="material-icons">person</Icon>
+					</div>
+				</Cell>
+				<Cell span={11}>
+					<input class="center" style="border: 0; padding-top: 5px;" placeholder="Email" />
+				</Cell>
+			</LayoutGrid>
+		</div>
+
+		<div style="margin-bottom: 10px; margin-top: 20px;">
+			<p2>Password</p2>
+		</div>
+
+		<div class="input-field">
+			<LayoutGrid>
+				<Cell span={1}>
+					<div class="center-stuff">
+						<Icon class="material-icons">key</Icon>
+					</div>
+				</Cell>
+				<Cell span={11}>
+					<input class="center" style="border: 0; padding-top: 5px;" placeholder="Password" />
+				</Cell>
+			</LayoutGrid>
+		</div>
+
+		<button class="input-field" style="margin-top: 49.5px; background-color: #51B198;">
+			<p style="font-size: 20px;">Login</p>
+		</button>
+	</div>
+</div>
+
+<BottomAppBar bind:this={bottomAppBar} color={'primary'}>
+	<Section>
+		<!-- <IconButton class="material-icons">menu</IconButton> -->
+		<p2 class="center-stuff" style="font-size: 90%;">Contact | About</p2>
+	</Section>
+	<Section>
+		<IconButton class="material-icons" aria-label="Search">search</IconButton>
+		<IconButton class="material-icons" aria-label="More">more_vert</IconButton>
+	</Section>
+</BottomAppBar>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap');
+	.input-field {
+		background-color: white;
+		width: 100%;
+		height: 60px;
+		border-radius: 10px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	input:focus {
+		outline: 0px solid transparent;
+	}
+	.center-stuff {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	p {
+		color: #ffffff;
+		font-family: 'Inter', sans-serif;
+		font-weight: 800;
+		font-size: 32px;
+	}
+	p2 {
+		color: #ffffff;
+		font-family: 'Inter', sans-serif;
+		font-weight: 400;
+		font-size: 16px;
+	}
+	:global(body) {
+		background-color: red;
+	}
+	.login {
+		border-radius: 30px;
+		background-color: #130f42;
+		height: 459.5px;
+		width: 500px;
+		display: flex;
+	}
+	.centered {
+		position: fixed;
+		top: 45%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	.column {
+		flex: 50%;
+	}
+</style>
