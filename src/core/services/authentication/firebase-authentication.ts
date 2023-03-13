207b2f2967042ee7a420b37e9db0a3cf14bbe6d3
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-13 20:31:14 +0700

    feat: fix security

--- src/core/services/authentication/firebase-authentication.ts
+++ src/core/services/authentication/firebase-authentication.ts
@@ -7,25 +7,16 @@ import {
 	signOut
 } from 'firebase/auth';
 import { initializeApp, type FirebaseApp } from 'firebase/app';
-import type { FirebaseConfig } from '../../interfaces/firebase-config';
 import authStore from '../../store/auth-store';
 import { goto } from '$app/navigation';
+import firebaseConfig from '../../config/firebase-config.json';
 
 class FirebaseAuthentication {
 	private app?: FirebaseApp;
 	private auth?: FirebaseAuth;
-	private firebaseConfig: FirebaseConfig = {
-		apiKey: 'AIzaSyCphoxGbZ4Ts-YCDH54Zhl6no2FjTTyTyI',
-		authDomain: 'jappl-authentication.firebaseapp.com',
-		projectId: 'jappl-authentication',
-		storageBucket: 'jappl-authentication.appspot.com',
-		messagingSenderId: '803380547350',
-		appId: '1:803380547350:web:0899b5dbfb13c897dd0a37',
-		measurementId: 'G-PEWBXPDLJT'
-	};
 
-	public initApp() {
-		this.app = initializeApp(this.firebaseConfig);
+	public async initApp() {
+		this.app = initializeApp(firebaseConfig);
 		this.auth = getAuth(this.app);
 
 		onAuthStateChanged(this.auth, async (user) => {
