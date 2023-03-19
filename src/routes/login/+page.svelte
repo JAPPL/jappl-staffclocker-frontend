9b511707cfbb8dfa3d7dbb51785954ebedc9e21a
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-19 18:23:40 +0700

    fix: login card

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Button from '@smui/button';
+	import Button, { Label } from '@smui/button';
+	import Card from '@smui/card';
 	import BottomAppBar, { Section } from '@smui-extra/bottom-app-bar';
 	import { loginWithGoogle } from '../../lib/firebase/firebase-client';
 	import { goto } from '$app/navigation';
@@ -35,27 +36,40 @@
 	}
 </script>
 
-<div>
-	<div class="login centered">
-		<div class="column" style="margin-top: 50px; margin-left: 10%; margin-right: 10%;">
-			<h1 style="margin-bottom: 15px;">Welcome to JAPPL</h1>
+<div class="full-screen-size">
+	<img src="src/lib/images/logo-dark.png" class="logo-size" alt="logo" />
+	<div class="center">
+		<div class="login">
+			<div class="login-card-content">
+				<h1 style="margin-bottom: 15px;">Welcome to JAPPL</h1>
 
-			<p>Options</p>
+				<p>Options</p>
 
-			<button on:click={login} class="input-field" style="margin-top: 5px; background-color: red;">
-				<p style="font-size: 20px;">Login with Google</p>
-			</button>
+				<div class="test">
+					<Button
+						style="
+							background-color: #FF6B6B;
+							border-radius: 30px;
+							width: 100%
+						"
+						variant="raised"
+						ripple={false}
+						on:click={login}
+					>
+						<img src="src/lib/images/google.png" class="google-logo" alt="logo" />
+						<Label>Login with Google</Label>
+					</Button>
+				</div>
 
-			<p>if you can not login, please contact admin.</p>
+				<p style="margin-bottom: 0">if you can not login, please contact admin.</p>
+			</div>
 		</div>
 	</div>
 
-	<Button variant="raised" class="button-shaped-round">Login with Google</Button>
-
 	<BottomAppBar bind:this={bottomAppBar}>
 		<Section>
-			<!-- <IconButton class="material-icons">menu</IconButton> -->
-			<p class="center-stuff" style="font-size: 90%;">Contact | About</p>
+			<!--			 <IconButton class="material-icons">menu</IconButton>-->
+			<p>Contact | About</p>
 		</Section>
 	</BottomAppBar>
 </div>
@@ -63,26 +77,33 @@
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap');
 
-	.input-field {
-		background-color: white;
-		width: 100%;
-		height: 60px;
-		border-radius: 10px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
+	.full-screen-size {
+		min-height: 90vh;
 	}
 
-	.center-stuff {
+	.logo-size {
+		width: 137px;
+		height: 100%;
+	}
+
+	.google-logo {
+		width: 25px;
+		height: fit-content;
+		margin-right: 10px;
+	}
+
+	.center {
 		display: flex;
 		justify-content: center;
-		align-items: center;
+	}
+
+	.login-card-content {
+		margin: 60px 10% 10% 10%;
 	}
 
 	p {
 		color: #ffffff;
 		font-family: 'Inter', sans-serif;
-		/*font-style: normal;*/
 		font-weight: 400;
 	}
 
@@ -96,17 +117,7 @@
 	.login {
 		border-radius: 30px;
 		background-color: #130f42;
-		min-height: 300px;
-		min-width: 500px;
-		display: flex;
-	}
-	.centered {
-		position: fixed;
-		top: 45%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
-	.column {
-		flex: 50%;
+		min-height: 270px;
+		min-width: 400px;
 	}
 </style>
--- src/routes/timelog/+page.server.ts
+++ src/routes/timelog/+page.server.ts
@@ -4,6 +4,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ locals }) => {
 	// redirect user if not logged in
 	if (!locals.token) {
+		console.log('access deny');
 		throw redirect(303, '/login');
 	}
 };
