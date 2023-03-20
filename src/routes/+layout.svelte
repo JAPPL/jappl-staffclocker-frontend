2df876ff5b250cdaef58ec09f09ef68790c542d5
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-20 23:23:31 +0700

    feat: add menu tooltip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,26 @@
 <script lang="ts">
 	import { Toaster } from 'svelte-french-toast';
+	import Tooltip, { Wrapper } from '@smui/tooltip';
 	import Drawer, { AppContent, Content } from '@smui/drawer';
-	import List, { Item, Text } from '@smui/list';
+	import List, { Item } from '@smui/list';
+	import { Icon } from '@smui/common';
 
 	let open = true;
 </script>
 
 <Toaster />
 <div>
-	<Drawer color="primary" variant="dismissible" bind:open>
+	<Drawer variant="dismissible" bind:open>
 		<div class="center">
 			<img src="src/lib/images/logo-without-text.png" class="logo-size" alt="logo" />
 		</div>
 		<Content>
 			<List>
 				<Item>
-					<Text>Gray Kittens</Text>
-				</Item>
-				<Item>
-					<Text>A Space Rocket</Text>
-				</Item>
-				<Item>
-					<Text>100 Pounds of Gravel</Text>
-				</Item>
-				<Item>
-					<Text>All of the Shrimp</Text>
-				</Item>
-				<Item>
-					<Text>Gray Kittens</Text>
-				</Item>
-				<Item>
-					<Text>A Space Rocket</Text>
-				</Item>
-				<Item>
-					<Text>100 Pounds of Gravel</Text>
-				</Item>
-				<Item>
-					<Text>All of the Shrimp</Text>
+					<Wrapper>
+						<Icon style="color: white" class="material-icons">home</Icon>
+						<Tooltip xPos="start">Dashboard.</Tooltip>
+					</Wrapper>
 				</Item>
 			</List>
 		</Content>
@@ -59,7 +43,8 @@
 		padding-top: 8px;
 		padding-bottom: 8px;
 		border-radius: 50px;
-		width: 60px;
+		background: #130f42;
+		width: 57px;
 	}
 
 	* :global(.mdc-drawer-app-content) {
