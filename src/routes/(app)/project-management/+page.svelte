0fb4ec3ad07dd72907120eb8a70aa3f47a13d30e
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-26 17:40:29 +0700

    feat: project management mock ui

--- src/routes/(app)/project-management/+page.svelte
+++ src/routes/(app)/project-management/+page.svelte
@@ -1,17 +1,71 @@
 <script lang="ts">
 	import Card from '@smui/card';
+	import Icon from 'mdi-svelte';
+	import ProjectDialog from './ProjectDialog.svelte';
+	import LinearProgress from '@smui/linear-progress';
+	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+	import { mdiFolderPlus } from '@mdi/js';
+	import Button, { Label } from '@smui/button';
+
+	let loading = false;
+	let loadingDialog = true;
+	let openCreateDialog = true;
+
+	function toggleDialog(): void {
+		openCreateDialog = !openCreateDialog;
+	}
 </script>
 
 <div class="container">
 	<Card style="padding: 20px">
-		<h2 class="mdc-typography--headline6" style="margin: 0;">Project Management</h2>
+		<div style="display: flex; justify-content: space-between">
+			<h2 class="mdc-typography--headline6" style="margin: 0;">Project Management</h2>
+			<Button disabled={!loading} color="secondary" variant="unelevated" on:click={toggleDialog}>
+				<Icon path={mdiFolderPlus} />
+				<Label style="margin-left: 5px">Create project</Label>
+			</Button>
+		</div>
 		<div class="divider" />
+		<DataTable table$aria-label="Project List" style="width: 100%;">
+			<Head>
+				<Row>
+					<Cell numeric>ID</Cell>
+					<Cell style="width: 100%;">Name</Cell>
+					<Cell />
+					<Cell />
+				</Row>
+			</Head>
+			<Body>
+				<!--{#each items as item (item.id)}-->
+				<!--	<Row>-->
+				<!--		<Cell numeric>{item.id}</Cell>-->
+				<!--		<Cell>{item.name}</Cell>-->
+				<!--		<Cell>{item.username}</Cell>-->
+				<!--		<Cell>{item.email}</Cell>-->
+				<!--	</Row>-->
+				<!--{/each}-->
+			</Body>
+
+			<LinearProgress
+				indeterminate
+				bind:closed={loading}
+				aria-label="Data is being loaded..."
+				slot="progress"
+			/>
+		</DataTable>
 	</Card>
+
+	<ProjectDialog bind:loadingDialog bind:openCreateDialog />
 </div>
 
 <style lang="css">
 	.container:global(.mdc-card) {
 		padding: 20px;
+	}
+
+	.dialog-divider {
+		margin: 2px 0;
+		border-bottom: 1px solid #bfbfbf;
 	}
 
 	.divider {
---
+++ src/routes/(app)/project-management/ProjectDialog.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import Dialog, { Title, Content, Actions } from '@smui/dialog';
+	import LinearProgress from '@smui/linear-progress';
+	import Textfield from '@smui/textfield';
+	import Button, { Label } from '@smui/button';
+
+	export let loadingDialog = true;
+	export let openCreateDialog = false;
+	let projectName = '';
+
+	async function closeHandler(e: CustomEvent<{ action: string }>) {
+		switch (e.detail.action) {
+			case 'submit':
+				openCreateDialog = true;
+				await submitProjectForm();
+				break;
+			case 'close':
+				projectName = '';
+		}
+	}
+
+	function delay(ms: number) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+
+	async function submitProjectForm(): Promise<void> {
+		loadingDialog = false;
+		await delay(3000);
+		loadingDialog = true;
+		// openCreateDialog = false;
+	}
+</script>
+
+<Dialog
+	bind:open={openCreateDialog}
+	aria-labelledby="mandatory-title"
+	aria-describedby="mandatory-content"
+	scrimClickAction=""
+	escapeKeyAction=""
+	on:SMUIDialog:closed={closeHandler}
+	surface$style="min-width: calc(100vw - 60vw); max-width: calc(100vw - 100px);"
+>
+	<Title id="mandatory-title">Create project</Title>
+	{#if loadingDialog}
+		<div class="dialog-divider" />
+	{/if}
+	<LinearProgress bind:closed={loadingDialog} indeterminate />
+	<Content id="mandatory-content">
+		<Textfield
+			disabled={!loadingDialog}
+			label="Project Name"
+			variant="outlined"
+			required
+			bind:value={projectName}
+			input$placeholder="Ex: Project 1"
+			style="width: 100%"
+			helperLine$style="width: 100%;"
+		/>
+	</Content>
+
+	<Actions>
+		<Button action="close">
+			<Label>Close</Label>
+		</Button>
+		<Button color="secondary" variant="unelevated" action="submit">
+			<Label>Confirm</Label>
+		</Button>
+	</Actions>
+</Dialog>
+
+<style lang="css">
+	.dialog-divider {
+		margin: 2px 0;
+		border-bottom: 1px solid #bfbfbf;
+	}
+</style>
