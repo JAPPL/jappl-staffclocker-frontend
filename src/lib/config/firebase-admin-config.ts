9e8fb9a9be093c0e15c0b8f950fb4f16d5c63779
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-04-03 12:39:33 +0700

    fix: update sonar property

--- src/lib/config/firebase-admin-config.ts
+++ src/lib/config/firebase-admin-config.ts
@@ -7,8 +7,6 @@ if (
 	throw new Error('Firebase admin config environment variables not given');
 }
 
-console.log('test ', import.meta.env.VITE_FIREBASE_ADMIN_PRIVATE_KEY);
-
 export const firebaseAdminConfig = {
 	type: import.meta.env.VITE_FIREBASE_ADMIN_TYPE,
 	projectId: import.meta.env.VITE_FIREBASE_ADMIN_PROJECTID,
