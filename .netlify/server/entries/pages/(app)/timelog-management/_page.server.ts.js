c0e89be01d5118cf3948c079b8357889b96425ee
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-04-11 00:59:34 +0700

    fix: remove build directory

--- .netlify/server/entries/pages/(app)/timelog-management/_page.server.ts.js
+++
@@ -1,8 +0,0 @@
-import { r as redirect } from '../../../../chunks/index.js';
-const load = async ({ locals }) => {
-	if (!locals.token) {
-		console.log('access deny');
-		throw redirect(303, '/login');
-	}
-};
-export { load };
