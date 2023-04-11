1bf997995e7525bce07c907154a5bf472543e463
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-04-11 20:41:43 +0700

    fix: fix CORS error

--- src/routes/(app)/project-management/+page.svelte
+++ src/routes/(app)/project-management/+page.svelte
@@ -43,7 +43,7 @@
 	async function loadProject(): Promise<void> {
 		loading = false;
 		const token: string = $userStore.token || '';
-		await fetch('https://staffclocker.azurewebsites.net/api/project/', {
+		await fetch('api/project/', {
 			method: 'GET',
 			headers: new Headers({ Authorization: `Bearer ${token}` })
 		})
