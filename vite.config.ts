90379a421930fd8d96334c26cf7aee5cd029d87c
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-04-11 18:10:59 +0700

    fix: change proxy config for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,54 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+// import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['./setupTest.js'],
-		include: ['src/**/*.{timelog,spec}.{js,ts}'],
-		coverage: {
-			provider: 'c8',
-			exclude: ['src/**/*-config.ts', 'src/**/*.{timelog,spec}.{js,ts}'],
-			reporter: ['lcov', 'text']
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+	const env: Record<string, string> = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [sveltekit()],
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: ['./setupTest.js'],
+			include: ['src/**/*.{timelog,spec}.{js,ts}'],
+			coverage: {
+				provider: 'c8',
+				exclude: ['src/**/*-config.ts', 'src/**/*.{timelog,spec}.{js,ts}'],
+				reporter: ['lcov', 'text']
+			},
+			deps: {
+				inline: [`@sveltejs/kit`]
+			}
 		},
-		deps: {
-			inline: [`@sveltejs/kit`]
+		server: {
+			proxy: {
+				'/api': env?.VITE_APP_BACKEND ? env.VITE_APP_BACKEND : 'http://127.0.0.1:8000'
+			}
 		}
-	},
-	server: {
-		proxy: {
-			'/api': 'http://127.0.0.1:8000'
-		}
-	}
+	};
 });
+
+// export default defineConfig({
+// 	plugins: [sveltekit()],
+// 	test: {
+// 		globals: true,
+// 		environment: 'jsdom',
+// 		setupFiles: ['./setupTest.js'],
+// 		include: ['src/**/*.{timelog,spec}.{js,ts}'],
+// 		coverage: {
+// 			provider: 'c8',
+// 			exclude: ['src/**/*-config.ts', 'src/**/*.{timelog,spec}.{js,ts}'],
+// 			reporter: ['lcov', 'text']
+// 		},
+// 		deps: {
+// 			inline: [`@sveltejs/kit`]
+// 		}
+// 	},
+// 	server: {
+// 		proxy: {
+// 			'/api': 'https://staffclocker.azurewebsites.net'
+// 		}
+// 	}
+// });
