150e41aa8d5a8344ef00d28968b1b2715d290dfe
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-04-11 19:42:11 +0700

    fix: add proxy redirection

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-// import { defineConfig } from 'vitest/config';
-
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
@@ -29,26 +27,3 @@ export default defineConfig(({ mode }) => {
 		}
 	};
 });
-
-// export default defineConfig({
-// 	plugins: [sveltekit()],
-// 	test: {
-// 		globals: true,
-// 		environment: 'jsdom',
-// 		setupFiles: ['./setupTest.js'],
-// 		include: ['src/**/*.{timelog,spec}.{js,ts}'],
-// 		coverage: {
-// 			provider: 'c8',
-// 			exclude: ['src/**/*-config.ts', 'src/**/*.{timelog,spec}.{js,ts}'],
-// 			reporter: ['lcov', 'text']
-// 		},
-// 		deps: {
-// 			inline: [`@sveltejs/kit`]
-// 		}
-// 	},
-// 	server: {
-// 		proxy: {
-// 			'/api': 'https://staffclocker.azurewebsites.net'
-// 		}
-// 	}
-// });
